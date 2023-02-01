9f52370f45920e55ed48605ed8325cc9c82d261b
rodrigo <barbas010289@gmail.com>
2023-02-01 12:25:41 -0600

    creacion de index y carrito

---
+++ js/main.js
@@ -0,0 +1,186 @@
+//Productos
+
+const productos = [
+    {
+        id: "abrigo-01",
+        titulo: "abrigo-01",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-02",
+        titulo: "abrigo-02",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-03",
+        titulo: "abrigo-03",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-04",
+        titulo: "abrigo-04",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "abrigo-05",
+        titulo: "abrigo-05",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    //camisetas
+    {
+        id: "camiseta-01",
+        titulo: "camiseta-01",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-02",
+        titulo: "camiseta-02",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-03",
+        titulo: "camiseta-03",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-04",
+        titulo: "camiseta-04",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-05",
+        titulo: "camiseta-05",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-06",
+        titulo: "camiseta-06",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-07",
+        titulo: "camiseta-07",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "camiseta-08",
+        titulo: "camiseta-08",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    //pantalones
+    {
+        id: "pantalon-01",
+        titulo: "pantalon-01",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-02",
+        titulo: "pantalon-02",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-03",
+        titulo: "pantalon-03",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-04",
+        titulo: "pantalon-04",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+    {
+        id: "pantalon-05",
+        titulo: "pantalon-05",
+        imagen:"../img/burro de machca receta.webp",
+        categoria: {
+            nombre: "abrigos",
+            id: "abrigos"
+        },
+        precio: 1000
+    },
+];
